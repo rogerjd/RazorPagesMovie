0eb5e97dcbccf3b42032987c3ff4ecdb1dd8a63a
rjpd@netzero.com <rogerjd>
2019-03-16 20:53:08 -0400

    Razor Handlers, wip

--- RazorPagesMovie/Pages/Test.cshtml.cs
+++ RazorPagesMovie/Pages/Test.cshtml.cs
@@ -9,8 +9,33 @@ namespace RazorPagesMovie.Pages
 {
     public class TestModel : PageModel
     {
+        private readonly RazorPagesMovie.Models.RazorPagesMovieContext _context;
+
+        public string Message { get; set; } = "Initial Request";
+
+
+        public TestModel(RazorPagesMovie.Models.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
         public void OnGet()
         {
+        }
+
+        public void OnPost()
+        {
+            Message = "Form Posted";
+        }
+
+        public void OnPostEdit()
+        {
+            Message = "Edit handler fired";
+        }
+
+        public void OnPostDelete()
+        {
+            Message = "Delete handler fired";
         }
     }
 }
